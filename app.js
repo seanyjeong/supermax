ccaa55049057256582d85ebb4e68dd857dc7b59a
maxsports <153889399+seanyjeong@users.noreply.github.com>
2024-05-15 18:32:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ const connection = mysql.createConnection({
   password: 'q141171616!',
   database: 'supermax',
   charset: 'utf8mb4'
-  
 });
 
 // HTTP 서버를 생성합니다.
 const server = http.createServer((req, res) => {
   // CORS 헤더를 설정합니다.
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '45.115.154.148');
 
   // 대학정보기본 테이블과 반영비율 테이블을 조인하는 쿼리를 작성합니다.
   const query = `
@@ -23,19 +22,17 @@ const server = http.createServer((req, res) => {
 
   connection.query(query, (err, rows, fields) => {
     if (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.end('Database query failed');
+      res.writeHead(500, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({ message: 'Database query failed', error: err }));
       return;
     }
-      // 각 컬럼의 이름을 가져옵니다.
-  const columnNames = fields.map(field => field.name);
 
-    // 응답 헤더에 'Content-Type'을 'text/html; charset=utf-8'로 설정합니다.
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    // 응답 헤더에 'Content-Type'을 'application/json'로 설정합니다.
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(rows));
   });
 });
 
 server.listen(3000, '0.0.0.0', () => {
-  console.log('Server running at https://0.0.0.0:3000/');
+  console.log('Server running at http://0.0.0.0:3000/');
 });
