20d60a322a50d11711688b31a7524225e5aaac10
maxsports <153889399+seanyjeong@users.noreply.github.com>
2024-05-17 17:42:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const https = require('https');
 const fs = require('fs');
-const connection = require('./db'); // db.js 파일을 불러옵니다.
+const mysql = require('mysql');
 
 // SSL/TLS 설정을 불러옵니다.
 const sslOptions = {
   key: fs.readFileSync('/etc/letsencrypt/live/supermax.kr/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/supermax.kr/fullchain.pem')
 };
+
+// 데이터베이스 연결을 설정합니다.
+var db_config = {
+  host: 'my8003.gabiadb.com',
+  user: 'maxilsan',
+  password: 'q141171616!',
+  database: 'supermax',
+  charset: 'utf8mb4'
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since the old one cannot be reused.
+
+  connection.connect(function(err) {              
+    if(err) {                                    
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  });                                     
+
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                        
+    } else {                                      
+      throw err;                                  
+    }
+  });
+}
+
+handleDisconnect();
 
 // HTTPS 서버를 생성합니다.
 const server = https.createServer(sslOptions, (req, res) => {
@@ -33,4 +66,4 @@ const server = https.createServer(sslOptions, (req, res) => {
 
 server.listen(3000, '0.0.0.0', () => {
   console.log('Server running at https://0.0.0.0:3000/');
-});
+}); 
