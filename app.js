c5ec03f7fb5d989ce4e9881119707f6f2f6b9a76
maxsports <153889399+seanyjeong@users.noreply.github.com>
2024-06-01 18:49:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -63,6 +63,9 @@ app.use(bodyParser.json());
 // CORS 헤더를 설정합니다.
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
