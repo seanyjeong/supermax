9c1316301185974d7629eda9c83775a22cc32f31
maxsports <153889399+seanyjeong@users.noreply.github.com>
2024-06-01 19:11:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -66,6 +66,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
