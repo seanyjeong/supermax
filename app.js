cf61b0038808d1358f0f0c1ec2f6aad7bbc5a788
maxsports <153889399+seanyjeong@users.noreply.github.com>
2024-07-10 17:12:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(session({
 
 // CORS 설정
 app.use(cors({
-  origin: ['https://supermax.co.kr','https://seanyjeong.github.io','https://chejump.com],
+  origin: ['https://supermax.co.kr','https://seanyjeong.github.io','https://chejump.com'],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
