83cd0c3ee251abaf293cdc3f3d0c0c93f6041bb9
maxsports <153889399+seanyjeong@users.noreply.github.com>
2024-07-26 19:31:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const requestIp = require('request-ip');
 const axios = require('axios');
-
 const app = express();
 const jwtSecret = 'your_jwt_secret'; // JWT 비밀키 설정
 const SESSION_TIMEOUT = 3600; // 세션 타임아웃을 1시간(3600초)으로 설정
-
 // 데이터베이스 연결을 설정합니다.
 const db_config = {
   host: '211.37.174.218',
@@ -21,19 +19,15 @@ const db_config = {
   database: 'supermax',
   charset: 'utf8mb4'
 };
-
 let connection;
-
 function handleDisconnect() {
   connection = mysql.createConnection(db_config);
-
   connection.connect(function (err) {
     if (err) {
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000);
     }
   });
-
   connection.on('error', function (err) {
     console.log('db error', err);
     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
@@ -43,9 +37,7 @@ function handleDisconnect() {
     }
   });
 }
-
 handleDisconnect();
-
 // 세션 설정
 app.use(session({
   secret: 'your_secret_key',
@@ -53,7 +45,6 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false, sameSite: 'None', maxAge: SESSION_TIMEOUT * 1000 } // 쿠키 설정 조정
 }));
-
 // CORS 설정
 app.use(cors({
   origin: ['https://supermax.co.kr','https://seanyjeong.github.io','https://chejump.com'],
@@ -61,91 +52,75 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(requestIp.mw({ attributeName: 'clientIp' }));
-
 // HTTP 서버를 생성합니다.
 const server = http.createServer(app);
-
-
-
 // 로그인 엔드포인트
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
-
   connection.query(query, [username, password], async (err, results) => {
     if (err) {
       console.error('Database query failed:', err);
       res.status(500).json({ message: 'Database query failed', error: err });
       return;
     }
-
     if (results.length > 0) {
       const user = results[0];
       const token = jwt.sign({ username: user.username, legion: user.legion }, jwtSecret, { expiresIn: '1h' });
-
       // IP 주소 및 로그인 지역 정보를 가져와서 세션에 저장
       const ip = req.clientIp;
       let location = 'Unknown';
-
       try {
-        const response = await axios.get(http://ip-api.com/json/${ip});
+        const response = await axios.get(`http://ip-api.com/json/${ip}`);
         const data = response.data;
-        location = ${data.city}, ${data.regionName}, ${data.country};
+        location = `${data.city}, ${data.regionName}, ${data.country}`;
       } catch (error) {
         console.error('Error fetching IP location:', error);
       }
-
       req.session.ip = ip;
       req.session.location = location;
       req.session.username = user.username;
       req.session.legion = user.legion;
-
       // 세션 정보를 데이터베이스에 저장
-      const insertSessionQuery = 
+      const insertSessionQuery = `
         INSERT INTO user_sessions (username, legion, ip, location)
         VALUES (?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE login_time = CURRENT_TIMESTAMP
-      ;
+      `;
       connection.query(insertSessionQuery, [user.username, user.legion, ip, location], (err, results) => {
         if (err) {
           console.error('Failed to insert session data:', err);
         }
       });
-
       res.status(200).json({ message: 'Login successful', token, username: user.username, legion: user.legion });
     } else {
       res.status(401).json({ message: 'Invalid credentials' });
     }
   });
 });
-
 // JWT 인증 미들웨어
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-
   if (token == null) return res.status(401).json({ message: 'No token provided' });
-
   jwt.verify(token, jwtSecret, (err, user) => {
     if (err) return res.status(403).json({ message: 'Invalid token' });
     req.user = user;
     next();
   });
 }
-
 // 현재 로그인한 사용자 정보를 가져오는 엔드포인트
 app.get('/admin', authenticateToken, (req, res) => {
   const username = req.user.username;
   if (username === 'sean8320') {
-    const query = 
+    const query = `
       SELECT username, legion, ip, location, login_time
       FROM user_sessions
       WHERE login_time > DATE_SUB(NOW(), INTERVAL ${SESSION_TIMEOUT} SECOND)
-    ;
+    `;
     connection.query(query, (err, results) => {
       if (err) {
         console.error('Failed to retrieve session data:', err);
@@ -158,7 +133,6 @@ app.get('/admin', authenticateToken, (req, res) => {
     res.status(403).json({ message: 'Access denied' });
   }
 });
-
 // 로그아웃 엔드포인트
 app.post('/logout', authenticateToken, (req, res) => {
   const username = req.user.username;
@@ -172,7 +146,6 @@ app.post('/logout', authenticateToken, (req, res) => {
     res.status(200).json({ message: 'Logout successful' });
   });
 });
-
 // '25정시' 데이터를 가져오는 엔드포인트
 app.get('/25jeongsi', authenticateToken, (req, res) => {
   const query = 'SELECT * FROM 25정시';
@@ -185,14 +158,13 @@ app.get('/25jeongsi', authenticateToken, (req, res) => {
     res.status(200).json(rows);
   });
 });
-
 // '25수시' 데이터를 가져오는 엔드포인트
 app.get('/25susi', authenticateToken, (req, res) => {
-  const query = 
+  const query = `
     SELECT s.*, i.image_data
     FROM 25수시 s
     LEFT JOIN images i ON s.id = i.id
-  ;
+  `;
   connection.query(query, (err, rows) => {
     if (err) {
       console.error('Database query failed:', err);
@@ -207,12 +179,10 @@ app.get('/25susi', authenticateToken, (req, res) => {
     res.status(200).json(rows);
   });
 });
-
 // 이미지 데이터를 Base64로 인코딩하여 클라이언트에 제공
 app.get('/image/:id', authenticateToken, (req, res) => {
   const imageId = req.params.id;
   const query = 'SELECT image_data FROM images WHERE id = ?';
-
   connection.query(query, [imageId], (err, rows) => {
     if (err) {
       console.error('Database query failed:', err);
@@ -227,21 +197,17 @@ app.get('/image/:id', authenticateToken, (req, res) => {
     }
   });
 });
-
 // 비밀번호 변경 엔드포인트
 app.post('/change-password', authenticateToken, (req, res) => {
   const { currentPassword, newPassword } = req.body;
   const username = req.user.username;
-
   const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
-
   connection.query(query, [username, currentPassword], (err, results) => {
     if (err) {
       console.error('Database query failed:', err);
       res.status(500).json({ message: 'Database query failed', error: err });
       return;
     }
-
     if (results.length > 0) {
       const updateQuery = 'UPDATE users SET password = ? WHERE username = ?';
       connection.query(updateQuery, [newPassword, username], (err, results) => {
@@ -257,7 +223,6 @@ app.post('/change-password', authenticateToken, (req, res) => {
     }
   });
 });
-
 // 새로운 엔드포인트 추가: 점수 저장
 app.post('/save-duniv', (req, res) => {
   const {
@@ -265,14 +230,11 @@ app.post('/save-duniv', (req, res) => {
     standingJump, weightedRun, backStrength, sitAndReach,
     academicScore, practicalTotal, totalScore // 실기총점 추가
   } = req.body;
-
-  const query = 
+  const query = `
     INSERT INTO dscores (name, academy, formType, gender, standingJump, weightedRun, backStrength, sitAndReach, academicScore, practicalTotal, totalScore)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-  ;
-
+  `;
   const values = [name, academy, formType, gender, standingJump, weightedRun, backStrength, sitAndReach, academicScore, practicalTotal, totalScore];
-
   connection.query(query, values, (err, results) => {
     if (err) {
       console.error('Failed to insert data:', err);
@@ -282,7 +244,6 @@ app.post('/save-duniv', (req, res) => {
     res.status(200).json({ message: 'Data inserted successfully' });
   });
 });
-
 // 수시 데이터 가져오기 엔드포인트에리
 app.get('/get-susi-data', (req, res) => {
     const query = "SELECT * FROM dscores WHERE formType='susi' ORDER BY totalScore DESC";
@@ -291,7 +252,6 @@ app.get('/get-susi-data', (req, res) => {
         res.json(results);
     });
 });
-
 // 정시 데이터 가져오기 엔드포인트
 app.get('/get-jeongsi-data', (req, res) => {
     const query = "SELECT * FROM dscores WHERE formType='jeongsi' ORDER BY totalScore DESC";
@@ -300,7 +260,6 @@ app.get('/get-jeongsi-data', (req, res) => {
         res.json(results);
     });
 });
-
 // 수시 데이터 저장 엔드포인트
 app.post('/save-ERICA-susi', (req, res) => {
     const {
@@ -308,12 +267,10 @@ app.post('/save-ERICA-susi', (req, res) => {
         standingJump, medicineBall, tenMeterRun,
         twentyFiveMeterRun, practicalScore, totalScore
     } = req.body;
-
-    const query = 
+    const query = `
         INSERT INTO huniv (name, academy, formType, gender, standingJump, medicineBall, tenMeterRun, twentyFiveMeterRun, practicalScore, totalScore)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ;
-
+    `;
     connection.query(query, [name, academy, formType, gender, standingJump, medicineBall, tenMeterRun, twentyFiveMeterRun, practicalScore, totalScore], (error, results) => {
         if (error) {
             console.error('Error saving data:', error);
@@ -323,7 +280,6 @@ app.post('/save-ERICA-susi', (req, res) => {
         }
     });
 });
-
 // 정시 데이터 저장 엔드포인트
 app.post('/save-ERICA-jeongsi', (req, res) => {
     const {
@@ -331,12 +287,10 @@ app.post('/save-ERICA-jeongsi', (req, res) => {
         standingJump, medicineBall, tenMeterRun,
         twentyFiveMeterRun, practicalScore, totalScore, suengScore
     } = req.body;
-
-    const query = 
+    const query = `
         INSERT INTO huniv (name, academy, formType, gender, standingJump, medicineBall, tenMeterRun, twentyFiveMeterRun, practicalScore, totalScore, suengScore)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ;
-
+    `;
     connection.query(query, [name, academy, formType, gender, standingJump, medicineBall, tenMeterRun, twentyFiveMeterRun, practicalScore, totalScore, suengScore], (error, results) => {
         if (error) {
             console.error('Error saving data:', error);
@@ -346,13 +300,10 @@ app.post('/save-ERICA-jeongsi', (req, res) => {
         }
     });
 });
-
 //가천대 데이터 저장 엔드포잉트
 app.post('/ga-save', (req, res) => {
   const { name, academy, formType, gender, schoolScore, strength, medicineBall, standingJump, tenMeterRun, practicalScore, totalScore } = req.body;
-
   const query = 'INSERT INTO gachon_scores (name, academy, formType, gender, schoolScore, strength, medicineBall, standingJump, tenMeterRun, practicalScore, totalScore) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
   connection.query(query, [name, academy, formType, gender, schoolScore, strength, medicineBall, standingJump, tenMeterRun, practicalScore, totalScore], (err, result) => {
     if (err) {
       console.error('Error saving data:', err);
@@ -396,6 +347,17 @@ app.get('/get-row-counts', (req, res) => {
 
 
 // 서버 시작
+
+    
+          
+            
+    
+
+          
+      
+    
+    
+  
 server.listen(3000, '0.0.0.0', () => {
   console.log('Server running at http://0.0.0.0:3000/');
 });
