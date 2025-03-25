570ed28c68549e768f0e09419ecba31e5182b5a6
maxsports <153889399+seanyjeong@users.noreply.github.com>
2025-03-26 03:04:06 +0900

    Update maxfeed.js

--- maxfeed.js
+++ maxfeed.js
@@ -289,7 +289,7 @@ app.post('/feed/user-info', (req, res) => {
                 return res.status(404).json({ error: "사용자를 찾을 수 없음" });
             }
 
-            const { name, profile_image, phone, birth_date, intro } = results[0];
+            const { name, profile_image, phone, birth_date, intro, gender } = results[0];
             const profileImgUrl = profile_image || "https://placehold.co/100x100";
 
             res.json({ 
