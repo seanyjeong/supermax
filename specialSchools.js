419ec23d4dd992141b1fa0d5698ee7ddc56fa43b
maxsports <153889399+seanyjeong@users.noreply.github.com>
2025-04-29 17:52:53 +0900

    Update specialSchools.js

--- specialSchools.js
+++ specialSchools.js
@@ -26,7 +26,10 @@ async function getKoreanHistoryData(대학학과ID) {
 const specialSchoolCalculators = {
   30: calculate강원대체육교육과,   // 강원대 체육교육과
   1: calculate강원대스포츠과학과, // 강원대 스포츠과학과
-  31: calculate강원대휴먼스포츠학부 // 강원대 휴먼스포츠학부
+  31: calculate강원대휴먼스포츠학부, // 강원대 휴먼스포츠학부
+  36: calculate공주대학교,//공주생체농어촌
+  37: calculate공주대학교,//공주대생체일반
+  38: calculate공주대학교//공주대체교
 };
 
 // ✨ 메인 SpecialSchool 계산기
@@ -89,9 +92,7 @@ async function calculate강원대스포츠과학과(studentScore, schoolInfo, en
   const 탐구1 = (studentScore.탐구1?.백분위 || 0);
   const 탐구2 = (studentScore.탐구2?.백분위 || 0);
   const 탐구평균 = (탐구1 + 탐구2) / 2; // 두 과목 점수 평균
-
   const 한국사 = koreanHistoryData[studentScore.한국사등급 - 1] || 0;  // 한국사 점수 계산
-
   const 합산 = 국어 + 탐구평균 + 높은수영;
   const 수능점수 = 합산 * 0.6;
 
@@ -108,32 +109,50 @@ async function calculate강원대휴먼스포츠학부(studentScore, schoolInfo,
   const 영어 = englishData[studentScore.영어등급 - 1] || 0;  // 영어 점수 계산
   const 탐구1 = studentScore.탐구1?.백분위 || 0;
   const 탐구2 = studentScore.탐구2?.백분위 || 0;
-
-  // 탐구1과 탐구2의 평균 점수 계산
   const 탐구평균 = (탐구1 + 탐구2) / 2;
-
-  // 국수영탐 중 잘 본 2개 과목을 선택
+// 국수영탐 중 잘 본 2개 과목을 선택
   const 후보 = [
     국어,
     수학,
     영어,
     탐구평균  // 탐구1과 탐구2 평균 점수 반영
   ];
-
-  후보.sort((a, b) => b - a); // 높은 점수 2개 선택
-
-  // 두 개의 높은 점수는 60% 반영, 나머지는 60% 반영
+ 후보.sort((a, b) => b - a); // 높은 점수 2개 선택
+// 두 개의 높은 점수는 60% 반영, 나머지는 60% 반영
   const 반영점수 = 후보[0] * 0.6 + 후보[1] * 0.6;
-
-  // 한국사 점수 계산
+// 한국사 점수 계산
   const 한국사 = koreanHistoryData[studentScore.한국사등급 - 1] || 0;
-
-  // 최종 점수 계산: 반영된 점수 + 한국사 점수
+ // 최종 점수 계산: 반영된 점수 + 한국사 점수
   const 수능점수 = 반영점수;
+ console.log('📚 [휴먼스포츠학부]', { 후보, 반영점수, 수능점수, 한국사 });
+return 수능점수 + 한국사;  // 최종 점수
+}
 
-  console.log('📚 [휴먼스포츠학부]', { 후보, 반영점수, 수능점수, 한국사 });
-
-  return 수능점수 + 한국사;  // 최종 점수
+//국수영탐(2) 비율60씩 두개 합산후 한국사.
+async function calculate공주대학교(studentScore, schoolInfo, englishData, koreanHistoryData) {
+  // 국어, 수학, 영어, 탐구 점수 계산
+  const 국어 = studentScore.국어?.백분위 || 0;
+  const 수학 = studentScore.수학?.백분위 || 0;
+  const 영어 = englishData[studentScore.영어등급 - 1] || 0;  // 영어 점수 계산
+  const 탐구1 = studentScore.탐구1?.백분위 || 0;
+  const 탐구2 = studentScore.탐구2?.백분위 || 0;
+  const 탐구MAX = Math.max(탐구1,탐구2);
+// 국수영탐 중 잘 본 2개 과목을 선택
+  const 후보 = [
+    국어,
+    수학,
+    영어,
+    탐구MAX  // 탐구1과 탐구2 잘본것
+  ];
+ 후보.sort((a, b) => b - a); // 높은 점수 2개 선택
+// 세개.
+  const 반영점수 = 후보[0]  + 후보[1] + 후보[2];
+// 한국사 점수 계산
+  const 한국사 = koreanHistoryData[studentScore.한국사등급 - 1] || 0;
+ // 최종 점수 계산: 반영된 점수 + 한국사 점수
+  const 수능점수 = (반영점수*8.5+150)*(schoolInfo.수능비율 /100 );
+ console.log('📚 [공주대]', { 후보, 반영점수, 수능점수, 한국사 });
+return 수능점수 + 한국사;  // 최종 점수
 }
 
 
