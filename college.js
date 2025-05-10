edcec2e16e4dbaf90150008d452fb3bf016f66f0
maxsports <153889399+seanyjeong@users.noreply.github.com>
2025-05-10 14:54:20 +0900

    Update college.js

--- college.js
+++ college.js
@@ -6,11 +6,12 @@ const port = 9000;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); 
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
+
 
 app.use(express.json());
 
