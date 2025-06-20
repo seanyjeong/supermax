468ae151a33ae227c0bac98178d083a8cef90b97
maxsports <153889399+seanyjeong@users.noreply.github.com>
2025-06-20 19:06:47 +0900

    Update 26susi.js

--- 26susi.js
+++ 26susi.js
@@ -4,6 +4,8 @@ const mysql = require('mysql2');
 const app = express();
 const port = 8080;
 
+app.use(cors());
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE'); 
