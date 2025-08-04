f3da8bef81cadce064100c94dd438c62e76b7e3b
maxsports <153889399+seanyjeong@users.noreply.github.com>
2025-08-04 14:28:13 +0900

    Update 26susi.js

--- 26susi.js
+++ 26susi.js
@@ -1136,7 +1136,7 @@ app.post('/26susi_student_grade_update', authJWT, async (req, res) => {
 // ✅ isReverse 판별 함수
 const isReverseEvent = (eventName) => {
   const lower = eventName.toLowerCase();
-  return ['10', '20', 'run', '100', 'z', '달리기','벽치기','런','에르고'].some(keyword => lower.includes(keyword));
+  return ['10', '20', 'run', '100', 'z', '달리기','벽치기','런','에르고','앞뒤구르기'].some(keyword => lower.includes(keyword));
 };
 
 // ✅ 1. 대학/학과 선택용 실기ID 목록
@@ -1229,7 +1229,7 @@ app.post('/26susi/calculate-final-score', authJWT, async (req, res) => {
             }
 
             const studentRecord = parseFloat(input.기록);
-            const reverse = ['10m', '20m', 'run', '100', 'z', '달리기','벽치기','런','에르고'].some(k => input.종목명.toLowerCase().includes(k));
+            const reverse = ['10m', '20m', 'run', '100', 'z', '달리기','벽치기','런','에르고','앞뒤구르기'].some(k => input.종목명.toLowerCase().includes(k));
 
             // ✅✅✅ 대학ID 155번(동국대) 특수 계산식 ✅✅✅
             if (Number(대학ID) === 155) {
@@ -1268,7 +1268,7 @@ app.post('/26susi/calculate-final-score', authJWT, async (req, res) => {
 
                 const benchmarkRecord = parseFloat(pf_row.기록);
                 const studentRecord = parseFloat(input.기록);
-                const reverse = ['10m', '20m', 'run', '100', 'z', '달리기','벽치기','런','에르고'].some(k => input.종목명.toLowerCase().includes(k));
+                const reverse = ['10m', '20m', 'run', '100', 'z', '달리기','벽치기','런','에르고','앞뒤구르기'].some(k => input.종목명.toLowerCase().includes(k));
 
                 if (reverse) { // 기록이 낮을수록 좋은 종목
                     return { 종목명: input.종목명, 배점: studentRecord <= benchmarkRecord ? 'P' : 'F' };
