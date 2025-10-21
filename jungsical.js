9afc2923e98b8a034eafc3ba41087e6a756dcd45
maxsports <153889399+seanyjeong@users.noreply.github.com>
2025-10-21 13:49:27 +0900

    Update jungsical.js

--- jungsical.js
+++ jungsical.js
@@ -111,7 +111,7 @@ function buildSpecialContext(F, S, highestMap) {
 
   const korKey  = kmSubjectNameForKorean(S?.국어);
   const mathKey = kmSubjectNameForMath(S?.수학);
-  const kme_pct = [ctx.kor_pct, ctx.math_pct, ctx.eng_pct_est].sort((a, b) => b - a); ctx.top2_sum_raw_pct_kme = (kme_pct[0] || 0) + (kme_pct[1] || 0);
+ 
 
   // 1) 기본값
   let korMax  = 200;
@@ -366,7 +366,10 @@ ctx.ratio_inq  = Number(F['탐구'] || 0);
 
 
 
-
+// 국어, 수학, 영어(추정) 백분위 계산 후 상위 2개 합 계산
+const kme_pct = [ctx.kor_pct, ctx.math_pct, ctx.eng_pct_est].sort((a, b) => b - a);
+ctx.top2_sum_raw_pct_kme = (kme_pct[0] || 0) + (kme_pct[1] || 0);
+log.push(`[특수공식 준비] 국/수/영(추정) 백분위: ${ctx.kor_pct}/${ctx.math_pct}/${ctx.eng_pct_est.toFixed(2)} -> 상위 2개 합: ${ctx.top2_sum_raw_pct_kme}`); // 확인용 로그 추가 (선택사항)
 
 
   return ctx;
